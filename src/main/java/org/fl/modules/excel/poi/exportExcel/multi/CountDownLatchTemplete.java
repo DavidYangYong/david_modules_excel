0f88becd9a1379d863ce4546d9555a8328c5ee36
DavidYangYong <yyzhpq@163.com>
2020-05-06 09:07:22 +0800

    add synchronized

--- src/main/java/org/fl/modules/excel/poi/exportExcel/multi/CountDownLatchTemplete.java
+++ src/main/java/org/fl/modules/excel/poi/exportExcel/multi/CountDownLatchTemplete.java
@@ -14,6 +14,10 @@
  * # * CountDownLatchDemo1.java Create on 2013-11-14 上午09:51:16 # * project Test # * Copyright 2013 by . #
  *
  * 文件名：CountDownLatchDemo1.java 版本信息： 日期：2013-11-14 Copyright 足下 Corporation 2013 版权所有
+ *
+ * 文件名：CountDownLatchDemo1.java 版本信息： 日期：2013-11-14 Copyright 足下 Corporation 2013 版权所有
+ *
+ * 文件名：CountDownLatchDemo1.java 版本信息： 日期：2013-11-14 Copyright 足下 Corporation 2013 版权所有
  */
 /**
  * 文件名：CountDownLatchDemo1.java
@@ -56,9 +60,12 @@ import org.fl.modules.utils.RowSelect;
  */
 @Slf4j
 class CountDownLatchTemplete {
+
 	private boolean isClose;
 
-
+	public boolean isClose() {
+		return isClose;
+	}
 
 	/***
 	 * 多线程主方法
@@ -97,23 +104,23 @@ class CountDownLatchTemplete {
 		sxssfWorkBookOperation.setSheetNum(pageNo);
 		// 设置特定的线程池，大小为pageNo
 		ExecutorService exe = Executors.newFixedThreadPool(pageNo, new NamedThreadFactory());
-		for (int i = 1; i <= pageNo; i++) {
-			RowSelect rowSelect = new RowSelect(i, pageSize, totalRows);
-
-			ThreadTemplete threadTemplete = new ThreadTemplete(
-					sxssfWorkBookOperation, rowSelect, sxssfWorkBookList,
-					sxssfWorkBookOperation.getPageSize());
-			Future future = exe.submit(threadTemplete);
-			doneCdl.countDown();
-		}
-		long start = 0;
-		if (log.isDebugEnabled()) {
-			// 记录所有连接线程的开始时间
-			start = System.currentTimeMillis();
-		}
-
 		// 所有线程虽然都已建立，并 start。但只有等闸门打开才都开始运行。
 		try {
+			for (int i = 1; i <= pageNo; i++) {
+				RowSelect rowSelect = new RowSelect(i, pageSize, totalRows);
+
+				ThreadTemplete threadTemplete = new ThreadTemplete(
+						sxssfWorkBookOperation, rowSelect, sxssfWorkBookList,
+						sxssfWorkBookOperation.getPageSize());
+				Future future = exe.submit(threadTemplete);
+				doneCdl.countDown();
+			}
+			long start = 0;
+			if (log.isDebugEnabled()) {
+				// 记录所有连接线程的开始时间
+				start = System.currentTimeMillis();
+			}
+
 			doneCdl.await();// 主线程等待所有连接结束
 			// 连接达到峰值后，执行一些测试逻辑代码
 			exe.shutdown();
@@ -122,6 +129,17 @@ class CountDownLatchTemplete {
 				loop = !exe.awaitTermination(30, TimeUnit.SECONDS);
 			} while (loop);
 			isClose = true;
+
+			if (log.isDebugEnabled()) {
+				// 记录所有连接线程的结束时间
+				long end = System.currentTimeMillis();
+				log.debug(
+						"countDownLatch(int, SXSSFWorkBookUtil, ISxssfWorkBookList) - The task takes time(ms): "
+								+ ((end - start) / 1000));
+				log.debug(
+						"countDownLatch(int, SXSSFWorkBookUtil, ISxssfWorkBookList) - CountDownLatchDemo1:"
+								+ new Date().toString());
+			}
 		} catch (InterruptedException e) {
 			log.error(
 					"countDownLatch(int, SXSSFWorkBookUtil, ISxssfWorkBookList)",
@@ -133,20 +151,5 @@ class CountDownLatchTemplete {
 			}
 		}
 
-		if (log.isDebugEnabled()) {
-			// 记录所有连接线程的结束时间
-			long end = System.currentTimeMillis();
-			log.debug(
-					"countDownLatch(int, SXSSFWorkBookUtil, ISxssfWorkBookList) - The task takes time(ms): "
-							+ ((end - start) / 1000));
-			log.debug(
-					"countDownLatch(int, SXSSFWorkBookUtil, ISxssfWorkBookList) - CountDownLatchDemo1:"
-							+ new Date().toString());
-		}
-
-	}
-
-	public boolean isClose() {
-		return isClose;
 	}
 }
